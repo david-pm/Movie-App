f770de233b5de13d4d5842fffc0e3ac6b0ff9780
David McDonald <dmcdonald@sellbrite.com>
2016-01-17 23:41:05 -0800

    movie result directive spec

---
+++ spec/movie-app/movie_result_directive_spec.js
@@ -0,0 +1,40 @@
+describe('Movie Result Directive', function() {
+
+  var result = {
+    "Title":"Star Wars: Episode IV - A New Hope",
+    "Released":"25 May 1977",
+    "Genre":"Action, Adventure, Fantasy",
+    "Director":"George Lucas",
+    "Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
+    "Poster":"http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX300.jpg",
+  };
+
+  var expectedHtml = [
+      '<div class="col-sm-4">',
+        '<img ng-src="http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX300.jpg" alt="Star Wars: Episode IV - A New Hope" width="220" src="http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX300.jpg">',
+      '</div>',
+      '<div class="col-sm-8">',
+        '<h3 class="ng-binding">Star Wars: Episode IV - A New Hope</h3>',
+        '<p class="ng-binding"><strong>Director:</strong> George Lucas</p>',
+        '<p class="ng-binding"><strong>Actors:</strong> Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing</p>',
+        '<p class="ng-binding"><strong>Released:</strong> 25 May 1977</p>',
+        '<p class="ng-binding"><strong>Genre:</strong> Action, Adventure, Fantasy</p>',
+      '</div>',
+  ].join('');
+  var $compile, $rootScope;
+
+  beforeEach(module('movieApp'));
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('outputs movie results to HTML', function() {
+    var element;
+    $rootScope.result = result; // set our data to the scope, which we pass into the $compile service
+    element = $compile('<movie-result result="result"></movie-result>')($rootScope);
+    $rootScope.$digest();
+    expect(element.html()).toBe(expectedHtml);
+  });
+
+});
